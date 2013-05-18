a2866c8c22a684d2016ece8bf7b19c2e5cf1fcc0
Yasen <yasen8707@abv.bg>
2013-05-18 14:17:35 +0300

    Update 03CompanyMan.cs

--- CSharp-Programing-Part-1/04.ConsoleInputOutput/03CompanyMan.cs
+++ CSharp-Programing-Part-1/04.ConsoleInputOutput/03CompanyMan.cs
@@ -10,23 +10,35 @@ class CompanyMan
 {
     static void Main()
     {
-        Console.Write("{0,-30} : ", "Enter company name");
+        Console.Write("{0,-35} : ", "Enter company name");
         string companyName = Console.ReadLine();
-        Console.Write("{0,30} : ", "Enter company address");
+        Console.Write("{0,-35} : ", "Enter company address");
         string companyAddress = Console.ReadLine();
-        Console.Write("{0,30} : ", "Enter company phone number");
+        Console.Write("{0,-35} : ", "Enter company phone number");
         string companyPhone = Console.ReadLine();
-        Console.Write("{0,30} : ", "Enter company fax number");
+        Console.Write("{0,-35} : ", "Enter company fax number");
         string companyFax = Console.ReadLine();
-        Console.Write("{0,30} : ", "Enter company web site");
+        Console.Write("{0,-35} : ", "Enter company web site");
         string companyWebSite = Console.ReadLine();
-        Console.Write("{0,30} : ", "Enter company manager first name");
+        Console.Write("{0,-35} : ", "Enter company manager first name");
         string managerFirstName = Console.ReadLine();
-        Console.Write("{0,30} : ", "Enter company manager last name");
+        Console.Write("{0,-35} : ", "Enter company manager last name");
         string managerLastName = Console.ReadLine();
-        Console.Write("{0,30} : ", "Enter company manager Age");
+        Console.Write("{0,-35} : ", "Enter company manager Age");
         int managerAge = int.Parse(Console.ReadLine());
-        Console.Write("{0,30} : ", "Enter company manager phone number");
+        Console.Write("{0,-35} : ", "Enter company manager phone number");
         string managerPhone = Console.ReadLine();
+        Console.WriteLine("{0, -20} | {1, -20} |", "Company", companyName);
+        Console.WriteLine("{0, -20} | {1, -20} |", "Company address", companyAddress);
+        Console.WriteLine("{0, -20} | {1, -20} |", "Company phone", companyPhone);
+        Console.WriteLine("{0, -20} | {1, -20} |", "Company fax", companyFax);
+        Console.WriteLine("{0, -20} | {1, -20} |", "Company web site", companyWebSite);
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("{0, -20} :", "Company manager");
+        Console.WriteLine("{0, -20} : {1, -20} |", "Manager first name", managerFirstName);
+        Console.WriteLine("{0, -20} : {1, -20} |", "Manager last name", managerLastName);
+        Console.WriteLine("{0, -20} : {1, -20} |", "Manager age", managerAge);
+        Console.WriteLine("{0, -20} : {1, -20} |", "Manager phone", managerPhone);
     }
 }
+
