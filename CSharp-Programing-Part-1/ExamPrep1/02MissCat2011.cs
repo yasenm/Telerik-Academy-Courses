06e7ebd920b37250502b34faad3ff7bf9d997e34
Yasen <yasen8707@abv.bg>
2013-05-26 23:41:44 +0300

    Update 02MissCat2011.cs

--- CSharp-Programing-Part-1/ExamPrep1/02MissCat2011.cs
+++ CSharp-Programing-Part-1/ExamPrep1/02MissCat2011.cs
@@ -11,9 +11,9 @@ namespace _02.MissCat2011
         {
             int N = int.Parse(Console.ReadLine());
             int[] jury = new int[N];
-            int counter = 0;
-            int biggestCounter = 0;
-            byte finalResult = 0;
+            int[] resultArr = new int[10];
+            int finalResult = 0;
+            int result = 0;
 
             //----- Input all the jury's vote asked in an array afterwards to examin
             for (int i = 0; i < N; i++)
@@ -21,36 +21,22 @@ namespace _02.MissCat2011
                 byte inputNumber = byte.Parse(Console.ReadLine());
                 jury[i] = inputNumber;
             }
-            //----- Check which vote is the most common and chose it as result
-            for (int i = 0; i < jury.Length; i++)
+            for (int i = 0; i < N; i++)
             {
-                //----- Loop that checks how many times a given [i] element is met in the array of votes
-                for (int j = i; j < jury.Length; j++)
-                {
-                    //----- Every time we encounter the same element we count how many times we met it
-                    if (jury[i] == jury[j])
-                    {
-                        counter++;          // keeps the count of equal elements met
-                    }
-                }
-                //----- Check if we met some other element more times than the last one if so we update the
-                //----- biggest counter with it and check next time with it
-                if (counter > biggestCounter)
-                {
-                    biggestCounter = counter;
-                    finalResult = (byte)jury[i];
-                }
-                else if (counter == biggestCounter)
-                {
-                    var tempResult = (byte)jury[i];
-                    if ( tempResult < finalResult )
-                    {
-                        finalResult = tempResult;
-                    }
-                }
-                counter = 0;
+                var j = jury[i];
+                resultArr[j - 1] = resultArr[j - 1] + 1;
             }
-            Console.WriteLine(finalResult);
+            finalResult = resultArr[0];
+            result = 1;
+            for (int i = 0; i < resultArr.Length; i++)
+            {
+                if (finalResult < resultArr[i])
+                {
+                    finalResult = resultArr[i];
+                    result = i + 1;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
