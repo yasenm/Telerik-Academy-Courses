80bdb41eca7457be81313d9eef964175ec67b120
Yasen <yasen8707@abv.bg>
2013-05-26 18:13:42 +0300

    Update 02MissCat2011.cs

--- CSharp-Programing-Part-1/ExamPrep1/02MissCat2011.cs
+++ CSharp-Programing-Part-1/ExamPrep1/02MissCat2011.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.MissCat2011
 {
@@ -12,25 +11,21 @@ namespace _02.MissCat2011
         {
             int N = int.Parse(Console.ReadLine());
             int[] jury = new int[N];
-            int counter = 1;
+            int counter = 0;
             int biggestCounter = 0;
-            int result = 11;
-            int temp = 0;
+            byte finalResult = 0;
 
             //----- Input all the jury's vote asked in an array afterwards to examin
             for (int i = 0; i < N; i++)
             {
-                int inputNumber = int.Parse(Console.ReadLine());
-                if (inputNumber > 0 && inputNumber < 11)
-                {
-                    jury[i] = inputNumber;
-                }
+                byte inputNumber = byte.Parse(Console.ReadLine());
+                jury[i] = inputNumber;
             }
             //----- Check which vote is the most common and chose it as result
             for (int i = 0; i < jury.Length; i++)
             {
                 //----- Loop that checks how many times a given [i] element is met in the array of votes
-                for (int j = i + 1; j < jury.Length; j++)
+                for (int j = i; j < jury.Length; j++)
                 {
                     //----- Every time we encounter the same element we count how many times we met it
                     if (jury[i] == jury[j])
@@ -40,17 +35,22 @@ namespace _02.MissCat2011
                 }
                 //----- Check if we met some other element more times than the last one if so we update the
                 //----- biggest counter with it and check next time with it
-                if (counter >= biggestCounter)
+                if (counter > biggestCounter)
                 {
                     biggestCounter = counter;
-                    counter = 1;
-                    if (jury[i] < result)
+                    finalResult = (byte)jury[i];
+                }
+                else if (counter == biggestCounter)
+                {
+                    var tempResult = (byte)jury[i];
+                    if ( tempResult < finalResult )
                     {
-                        result = jury[i];
+                        finalResult = tempResult;
                     }
                 }
+                counter = 0;
             }
-            Console.WriteLine(result);
+            Console.WriteLine(finalResult);
         }
     }
 }
